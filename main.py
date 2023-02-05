2108d2b00ef69cf97b5c217313619f3418a8e133
Brandon Wright <psybw7@nottingham.ac.uk>
2023-02-05 16:28:16 +0000

    Add: Star function to LAC class

--- main.py
+++ main.py
@@ -13,7 +13,12 @@ class LAC:
         # Note: Returns ALL possible combinations not a set amount
         return list(itertools.product(*[x for x in argv]))
 
+    def _star(arr,elems):
+        #A* = {x₁,x₂,x₃...xₖ | k ≥ 0 ∧ x ∈ A}
+        return [x for x in arr if len(arr)-1 >= elems][:elems]
+
+
 f = [1,2,3,4,5,6,7,8]
 a = [10,20,30,40,50,60,70,80]
 
-print(LAC._concat(f,a,a,a,f))
+print(LAC._star(f,100))
